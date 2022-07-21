020d7a9104d0e41ad8518053d4bc705f65bc528f
Apk_boi <abraham.nnu@gmail.com>
2022-07-21 16:18:08 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/learn_urobo/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.learn_urobo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learn_urobo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
